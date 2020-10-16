0df6c64da870423162475df053f474f9bac85e94
sanghaarsha <sanghaarsha@gmail.com>
2020-10-16 17:17:20 +0545

    function intro

--- main folder/06. Functions/01. Course/01. Introduction/main.cpp
+++ main folder/06. Functions/01. Course/01. Introduction/main.cpp
@@ -5,8 +5,20 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+// Function Declaraion
+int sum(int, int);
+
 int main()
 {
-    cout << "Build Successfull" << endl;
+    int num1{0}, num2{0};
+    cout << "Enter two numbers to add : ";
+    cin >> num1 >> num2;
+    cout << "Sum = " << sum(num1, num2) << endl;
     return 0;
+}
+
+// Function Defination
+int sum(int n1, int n2)
+{
+    return n1 + n2;
 }
