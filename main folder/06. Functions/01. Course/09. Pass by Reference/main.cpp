af69d13006e2a4c282ed30115e9b23b0d290e006
sanghaarsha <sanghaarsha@gmail.com>
2020-10-17 14:28:12 +0545

    new

---
+++ main folder/06. Functions/01. Course/09. Pass by Reference/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+//Used objects from namespace 'std'
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+// Prototypes
+
+int main()
+{
+    cout << "Built";
+    return 0;
+}
+
+// Defination
