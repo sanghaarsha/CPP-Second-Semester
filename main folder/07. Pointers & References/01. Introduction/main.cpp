1ee8c1d0478dce104bdb006ef38a248a40184fad
sanghaarsha <sanghaarsha@gmail.com>
2020-10-23 09:38:56 +0545

    introduction

--- main folder/07. Pointers & References/01. Introduction/main.cpp
+++ main folder/07. Pointers & References/01. Introduction/main.cpp
@@ -7,6 +7,19 @@ using std::endl;
 
 int main()
 {
+    // Initializing to null to avoid garbage
+    int *intPtr{nullptr};
+    cout << intPtr << endl; // 0
+
+    // Intializing to other variables
+    int num{10};
+    int *numPtr{&num};
+    cout << num << endl;
+
+    // &num == numPtr, its because pointer is a variable of data type memory address, here its initalized to num, which means it holds memory location of the value stored in num
+
+    cout << numPtr << endl;
+    cout << &num << endl;
 
     return 0;
 }
